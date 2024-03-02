389a74acc0af9649490671c5320f16dce701a9b3
David Townsend <dttownsend@gmail.com>
2024-03-02 15:11:39 +0000

    Store ping data

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ping;
 use Illuminate\Http\Request;
 
 class PingController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
-
+        $ping = new Ping();
+        $ping->data = $request->all();
+        $ping->save();
     }
 }
--- app/Models/Ping.php
+++ app/Models/Ping.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ping extends Model
+/**
+ * @property int userId
+ * @property string data
+ */
+class Ping extends Model
 {
     use HasFactory;
 }
