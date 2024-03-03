772ffd59d924f482de57a0608763bb2755835302
David Townsend <dttownsend@gmail.com>
2024-03-03 12:53:21 +0000

    Have a go at ignoring pings without travel

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\DTOs\Geo;
+use App\Helpers\GeoHelper;
 use App\Models\Ping;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
 
 class PingController extends Controller
 {
     public function store(Request $request)
     {
-        $ping = new Ping();
-        $ping->data = json_encode($request->query());
-        $ping->lat = $request->get('lat');
-        $ping->lon = $request->get('lon');
-        $ping->save();
+        // Reject any pings that haven't moved since the last one
+        $lastPing = Ping::latest('created_at')->first();
+
+        $newPing = new Ping();
+        $newPing->data = json_encode($request->query());
+        $newPing->lat = $request->get('lat');
+        $newPing->lon = $request->get('lon');
+
+        if (GeoHelper::distance(new Geo($lastPing->lat, $lastPing->lon), new Geo($newPing->lat, $newPing->lon)) < env('MIN_TRAVEL')) {
+            return Response::HTTP_ALREADY_REPORTED;
+        }
+
+        $newPing->save();
     }
 }
--- app/Livewire/Map.php
+++ app/Livewire/Map.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 class Map extends Component
 {
     public Collection $pings;
-    public int $x = 1;
+
     public function render()
     {
         $this->pings = Ping::orderBy('created_at', 'DESC')->take(5)->get();
